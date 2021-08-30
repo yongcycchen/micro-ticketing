a64d38a0ecb7b63a3079d776838066711233ea2c
Yong Chen <yong@Yongs-MacBook-Air.local>
2021-08-30 15:31:37 -0700

    add auth react

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,6 +1,29 @@
 import "bootstrap/dist/css/bootstrap.css";
-
-const app = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import buildClient from "../api/build-client";
+import Header from "../component/header";
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <h1>Header </h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
-export default app;
+
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  // console.log(pageProps);
+  // console.log(data);
+  return {
+    pageProps,
+    ...data,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,11 +1,14 @@
-const LandingPage = ({ color }) => {
-  console.log("ifajdfkl", color);
-  return <h1>landing</h1>;
+import buildClient from "../api/build-client";
+const LandingPage = ({ currentUser }) => {
+  // console.log(currentUser);
+  return currentUser ? <h1>Yes</h1> : <h1>NO</h1>;
 };
 
-LandingPage.getInitialProps = () => {
-  console.log("i am on the server!");
-  return { color: "red" };
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+
+  return data;
 };
 
 export default LandingPage;
