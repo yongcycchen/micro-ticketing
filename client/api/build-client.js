34437e8b9d08ebd32d9fbe923694fb2185d2f2bc
Yong Chen <yong.cycchen@gmail.com>
2021-09-21 18:20:05 -0700

    change axios baseURL && Disable HTTPS Checking && add Digital Ocean Load Blancer

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,8 +5,8 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL:
-        "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
+      baseURL: "ticketing-yong.xyz",
+      // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
   } else {
