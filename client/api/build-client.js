ee44216613ff8197519b7cd60b0b8dfdf7b779cb
Yong Chen <yong.cycchen@gmail.com>
2021-09-21 19:02:08 -0700

    axios create baseURL changed

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ const buildClient = ({ req }) => {
     // We are on the server
 
     return axios.create({
-      baseURL: "ticketing-yong.xyz",
+      baseURL: "http://www.ticketing-yong.xyz",
       // "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local",
       headers: req.headers,
     });
